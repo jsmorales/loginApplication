be7180e891e8ce107cfbefb273c2d3751e538b4c
jsmorales <johan.morales@saiavh.com>
2018-12-21 12:38:19 -0500

    Se agregan pequeños ajustes de interfaz y se aumenta el timeout de respuesta.

--- app/src/main/java/com/example/johanmorales/loginapplication/Main2Activity.java
+++ app/src/main/java/com/example/johanmorales/loginapplication/Main2Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -21,6 +22,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -38,10 +40,13 @@ public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = Main2Activity.class.getSimpleName();
+    private static final int MY_SOCKET_TIMEOUT_MS = 20000;
     public TextView userNameTextViewToolBar;
     public TextView positionTextViewToolBar;
     public EditText socialNumberTextInput;
     public View consulta_progress;
+    public View consultaForm;
+    public View resultadoLayout;
 
     public Respuesta respuesta;
     public Resultado resultado;
@@ -73,12 +78,16 @@ public class Main2Activity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         consulta_progress = findViewById(R.id.consulta_progress);
+        consultaForm = findViewById(R.id.consultaForm);
+        resultadoLayout = findViewById(R.id.resultadoLayout);
 
         View hView =  navigationView.getHeaderView(0);
 
         userNameTextViewToolBar = hView.findViewById(R.id.userNameTextViewToolBar);
         positionTextViewToolBar = hView.findViewById(R.id.positionTextViewToolBar);
 
+
+        socialNumberTextInput = findViewById(R.id.socialNumberTextInput);
 
         respuesta = (Respuesta) getIntent().getExtras().get("respuesta");
 
@@ -111,87 +120,128 @@ public class Main2Activity extends AppCompatActivity
             @Override
             public void onClick(View v) {
 
-                consulta_progress.setVisibility(View.VISIBLE);
+                String socialNumber = socialNumberTextInput.getText().toString();
 
-                socialNumberTextInput = findViewById(R.id.socialNumberTextInput);
+                // Check for a valid password, if the user entered one.
+                if (TextUtils.isEmpty(socialNumber)) {
 
-                RequestQueue queue = Volley.newRequestQueue(Main2Activity.this);
+                    socialNumberTextInput.setError(getString(R.string.error_field_required));
+                    socialNumberTextInput.requestFocus();
 
-                String urlApi = "https://control-llegada-backend-test.azurewebsites.net/api/checkArriving";
+                }else{
 
-                JSONObject req = new JSONObject();
-
-                try {
-                    req.put("socialNumber",socialNumberTextInput.getText());
-                    req.put("filterUbication","CASINO");
-                    req.put("token",resultado.getToken());
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    consultar(socialNumber);
                 }
 
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                        (Request.Method.POST, urlApi, req, new Response.Listener<JSONObject>() {
 
-                            @Override
-                            public void onResponse(final JSONObject response) {
-
-                                //executeResponseJsonApp(response);
-
-                                consulta_progress.setVisibility(View.GONE);
-
-                                socialNumberTextInput.setText("");
-                                socialNumberTextInput.clearFocus();
-
-                                JSONObject res = response;
-
-                                Log.d(TAG, "Status de la respuesta: " + res.toString());
-
-                                TextView nameVerificationTextView = findViewById(R.id.nameVerificationTextView);
-                                TextView validationTextView = findViewById(R.id.validationTextView);
-                                TextView detailTextView = findViewById(R.id.detailTextView);
-                                TextView turnTextView = findViewById(R.id.turnTextView);
-
-                                try {
-
-                                    JSONObject result = response.getJSONObject("result");
-
-                                    nameVerificationTextView.setText(result.getString("name"));
-
-                                    validationTextView.setText(result.getBoolean("validation") ? "Puede Ingresar" : "No puede Ingresar");
-
-                                    detailTextView.setText(result.getString("detail"));
-
-                                    turnTextView.setText(result.getString("turn"));
-
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-                        }, new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // TODO: Handle error
-
-                                Log.d(TAG, error.toString());
-
-                                consulta_progress.setVisibility(View.GONE);
-
-                                //despliegue del error en un toast
-                                Toast.makeText(Main2Activity.this,error.toString(),Toast.LENGTH_LONG).show();
-
-                            }
-                        });
-
-                // Add the request to the RequestQueue.
-                queue.add(jsonObjectRequest);
 
             }
         });
 
 
         //-------------------------------------------------------------------------------
+    }
+
+    public void consultar(String socialNumber){
+
+        consulta_progress.setVisibility(View.VISIBLE);
+        consultaForm.setVisibility(View.GONE);
+        resultadoLayout.setVisibility(View.GONE);
+
+        RequestQueue queue = Volley.newRequestQueue(Main2Activity.this);
+
+        String urlApi = "https://control-llegada-backend-test.azurewebsites.net/api/checkArriving";
+
+        JSONObject req = new JSONObject();
+
+        try {
+            req.put("socialNumber",socialNumber);
+            req.put("filterUbication","CASINO");
+            req.put("token",resultado.getToken());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, urlApi, req, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(final JSONObject response) {
+
+                        //executeResponseJsonApp(response);
+
+                        consulta_progress.setVisibility(View.GONE);
+                        consultaForm.setVisibility(View.VISIBLE);
+                        resultadoLayout.setVisibility(View.VISIBLE);
+
+                        socialNumberTextInput.setText("");
+                        socialNumberTextInput.clearFocus();
+
+                        JSONObject res = response;
+
+                        Log.d(TAG, "Status de la respuesta: " + res.toString());
+
+                        TextView nameVerificationTextView = findViewById(R.id.nameVerificationTextView);
+                        TextView validationTextView = findViewById(R.id.validationTextView);
+                        TextView detailTextView = findViewById(R.id.detailTextView);
+                        TextView turnTextView = findViewById(R.id.turnTextView);
+
+                        try {
+
+                            if(res.getBoolean("success")) {
+
+                                JSONObject result = response.getJSONObject("result");
+
+                                nameVerificationTextView.setText(result.getString("name"));
+
+                                validationTextView.setText(result.getBoolean("validation") ? "Puede Ingresar" : "No puede Ingresar");
+
+                                detailTextView.setText(result.getString("detail"));
+
+                                turnTextView.setText(result.getString("turn"));
+
+                            }else{
+                                Toast.makeText(Main2Activity.this, res.getString("message"), Toast.LENGTH_SHORT).show();
+
+                                nameVerificationTextView.setText("-");
+
+                                validationTextView.setText("-");
+
+                                detailTextView.setText("-");
+
+                                turnTextView.setText("-");
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                        Log.d(TAG, error.toString());
+
+                        consulta_progress.setVisibility(View.GONE);
+
+                        //despliegue del error en un toast
+                        Toast.makeText(Main2Activity.this,error.toString(),Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
 
     @Override
